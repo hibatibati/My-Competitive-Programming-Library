3249087ace5b57bfe587caabd878a1a9523ab25b
hibati bati <larchandromeda@gmail.com>
2019-04-22 18:09:29 +0900

    Add Modulo

---
+++ Modulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Modulo
+{
+    private static int Multiple(int num1, int num2)
+        => (int)(BigMul(num1, num2) % Input.MOD);
+
+    public static int Pow(int m, int n)
+    {
+        if (n == 0) return 1;
+        if (n % 2 == 0) return Pow(Multiple(m, m), n / 2);
+        else return Multiple(Pow(Multiple(m, m), n / 2), m);
+    }
+
+    public static int Div(int a, int b)
+        => Multiple(a, Pow(b, Input.MOD - 2));
+
+    public class Combination
+    {
+        private int[] _fac;
+        public Combination(int num)
+        {
+            _fac = new int[num + 1];
+            _fac[0] = 1;
+            for (var i = 1; i <= num; i++)
+                _fac[i] = Multiple(_fac[i - 1], i);
+        }
+        public int Comb(int n, int r)
+        {
+            if (n < r) return 0;
+            if (n == r) return 1;
+            var calc = _fac[n];
+            calc = Div(calc, _fac[r]);
+            calc = Div(calc, _fac[n - r]);
+            return calc;
+        }
+        public int fac(int num)
+            => _fac[num];
+
+    }
+}
