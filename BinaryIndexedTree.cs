8d5de0f01a1339ad513e277ecf30092fcca4e731
hibati bati <larchandromeda@gmail.com>
2019-05-28 21:02:13 +0900

    Fix

--- BinaryIndexedTree.cs
+++ BinaryIndexedTree.cs
@@ -3,12 +3,16 @@
 public class BIT
 {
     int[] _item;
+    public BIT(int num)
+    {
+        _item = new int[num + 1];
+    }
     public int this[int index]
     {
         get { var s = 0; for (var i = index; i > 0; i -= i & -i) s += _item[i]; return s; }
     }
     public void add(int index, int value)
     {
-        for (var i = 0; i < _item.Length; i += i & -i) _item[i] += value;
+        for (var i = index; i <= _item.Length; i += i & -i) _item[i] += value;
     }
 }
