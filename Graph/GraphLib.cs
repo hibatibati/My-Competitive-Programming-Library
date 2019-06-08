1db7292e6aa6c173861361e73f6d897280707d66
hibati bati <larchandromeda@gmail.com>
2019-06-08 23:49:34 +0900

    add

---
+++ Graph/GraphLib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//グラフその他
+public class GraphLib
+{
+    //二部グラフ判定
+    public static bool IsBipartite(IList<IEnumerable<int>> edge, int index, int[] color, int c)
+    {
+        if (color[index] != 0) return color[index] == c;
+        color[index] = c;
+        var t = true;
+        foreach (var ad in edge[index])
+            t &= IsBipartite(edge, ad, color, 3 - c);
+        return t;
+    }
+}
