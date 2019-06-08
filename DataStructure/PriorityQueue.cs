4cf930e189b3345f409eb1208963671086b47228
hibati bati <larchandromeda@gmail.com>
2019-06-08 17:19:15 +0900

    add O(N)_Build method

--- DataStructure/PriorityQueue.cs
+++ DataStructure/PriorityQueue.cs
@@ -4,21 +4,32 @@ using System.Collections.Generic;
 /// <summary>
 /// Enqueue/Dequeue:O(logN)
 /// Peek:O(1)
+/// Build:O(N)
 /// </summary>
 /// <typeparam name="T">Comparableな型</typeparam>
 public class PriorityQueue<T> where T : IComparable<T>
 {
-    private List<T> _item;
+    public List<T> _item;
     public int Count { get { return _item.Count; } }
     public bool IsMaxHeap { get; set; }
     public T Peek { get { return _item[0]; } }
-    public PriorityQueue(bool IsMaxHeap = true) { _item = new List<T>(); this.IsMaxHeap = IsMaxHeap; }
+    public PriorityQueue(bool IsMaxHeap = true, IEnumerable<T> list = null)
+    {
+        _item = new List<T>();
+        this.IsMaxHeap = IsMaxHeap;
+        if (list != null)
+        {
+            _item.AddRange(list);
+            Build();
+        }
+    }
     private int Compare(int i, int j) => (IsMaxHeap ? 1 : -1) * _item[i].CompareTo(_item[j]);
+    private void Swap(int i, int j) { var t = _item[i]; _item[i] = _item[j]; _item[j] = t; }
     private int Parent(int i)
         => (i - 1) >> 1;
     private int Left(int i)
         => (i << 1) + 1;
-    public void Enqueue(T val)
+    public T Enqueue(T val)
     {
         int i = _item.Count;
         _item.Add(val);
@@ -26,12 +37,18 @@ public class PriorityQueue<T> where T : IComparable<T>
         {
             int p = Parent(i);
             if (Compare(i, p) > 0)
-            {
-                var tmp = _item[i];
-                _item[i] = _item[p];
-                _item[p] = tmp;
-            }
+                Swap(i, p);
             i = p;
+        }
+        return val;
+    }
+    private void Heapify(int index)
+    {
+        for (int i = index, j; (j = Left(i)) < _item.Count; i = j)
+        {
+            if (j != _item.Count - 1 && Compare(j, j + 1) < 0) j++;
+            if (Compare(i, j) < 0)
+                Swap(i, j);
         }
     }
     public T Dequeue()
@@ -39,17 +56,13 @@ public class PriorityQueue<T> where T : IComparable<T>
         T val = _item[0];
         _item[0] = _item[_item.Count - 1];
         _item.RemoveAt(_item.Count - 1);
-        for (int i = 0, j; (j = Left(i)) < _item.Count; i = j)
-        {
-            if (j != _item.Count - 1 && Compare(j, j + 1) < 0) j++;
-            if (Compare(i, j) < 0)
-            {
-                var tmp = _item[i];
-                _item[i] = _item[j];
-                _item[j] = tmp;
-            }
-        }
+        Heapify(0);
         return val;
+    }
+    private void Build()
+    {
+        for (var i = (_item.Count >> 1) - 1; i >= 0; i--)
+            Heapify(i);
     }
 }
 
