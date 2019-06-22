77eb9591303be991722aabf48f7bc04b407179aa
hibati bati <larchandromeda@gmail.com>
2019-06-22 15:47:07 +0900

    add LowLink

--- Graph/GraphLib.cs
+++ Graph/GraphLib.cs
@@ -96,4 +96,85 @@ public class GraphLib
             if (res[ad] == -1)
                 SCCrdfs(rev, ad, res, ct);
     }
+    
+    public class LowLink
+    {
+        private static List<int> arti;
+        private static int[] ord, low;
+        private static List<Pair<int, int>> bridge;
+        private static bool[] th;
+        private static IList<IEnumerable<int>> adj;
+        /// <summary>
+        /// 関節点（削除するとグラフの連結成分数が増加する頂点）を求めます
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        public static List<int> Articulation(IList<IEnumerable<int>> edge)
+        {
+            adj = edge;
+            arti = new List<int>();
+            ord = new int[edge.Count];
+            low = new int[edge.Count];
+            bridge = new List<Pair<int, int>>();
+            th = new bool[edge.Count];
+            var ct = 0;
+            for (var i = 0; i < edge.Count; i++)
+                if (!th[i]) ct = dfs(i, ct, -1);
+            return arti;
+        }
+        /// <summary>
+        /// 橋（削除するとグラフの連結成分数が増加する辺）を求めます
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        public static List<Pair<int, int>> Bridge(IList<IEnumerable<int>> edge)
+        {
+            Articulation(edge);
+            return bridge;
+        }
+        private static int dfs(int index, int ct, int par)
+        {
+            th[index] = true;
+            ord[index] = ct;
+            low[index] = ct++;
+            var isArti = false;
+            var c = 0;
+            foreach (var ad in adj[index])
+                if (!th[ad])
+                {
+                    c++;
+                    ct = dfs(ad, ct, index);
+                    low[index] = Min(low[index], low[ad]);
+                    isArti |= par != -1 && low[ad] >= ord[index];
+                    if (ord[index] < low[ad])
+                        bridge.Add(new Pair<int, int>(Min(index, ad), Max(index, ad)));
+                }
+                else if (ad != par)
+                    low[index] = Min(low[index], ord[ad]);
+            isArti |= par == -1 && c > 1;
+            if (isArti) arti.Add(index);
+            return ct;
+        }
+    }
 }
+
+
+class Pair<T1, T2> : IComparable<Pair<T1, T2>>
+{
+    public T1 v1 { get; set; }
+    public T2 v2 { get; set; }
+    public Pair() : this(default(T1), default(T2)) { }
+    public Pair(T1 v1, T2 v2)
+    { this.v1 = v1; this.v2 = v2; }
+
+    public int CompareTo(Pair<T1, T2> p)
+    {
+        var c = Comparer<T1>.Default.Compare(v1, p.v1);
+        if (c == 0)
+            c = Comparer<T2>.Default.Compare(v2, p.v2);
+        return c;
+    }
+    public override string ToString()
+        => $"{v1.ToString()} {v2.ToString()}";
+}
+
