a196f05f3e5fd0bf726ea3c5f7dad1335432dc02
hibati bati <larchandromeda@gmail.com>
2019-07-11 15:23:52 +0900

    add

---
+++ DataStructure/RingBuffer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RingBuffer<T>
+{
+    private T[] _item;
+    public int Count { get; private set; }
+    public RingBuffer(int size)
+    {
+        _item = new T[Count = size];
+    }
+    public T this[int index]
+    {
+        get { index %= Count; if (index < 0) index += Count; return _item[index]; }
+        set { index %= Count; if (index < 0) index += Count; _item[index] = value; }
+    }
+}
