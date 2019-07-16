87f8aee6a949f3475636dd9d0e47654dff5d2f5d
hibati bati <larchandromeda@gmail.com>
2019-07-16 17:46:38 +0900

    add

---
+++ DataStructure/PersistentStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class PersistentStack<T>
+{
+    public class Node
+    {
+        public T Key { get; set; }
+        public Node next { get; set; }
+        public Node(T key, Node next) { this.Key = key; this.next = next; }
+    }
+    public int Count { get; private set; }
+    public PersistentStack() { }
+    private PersistentStack(Node head, int Count = 0) { this.head = head; this.Count = Count; }
+    public Node head { get; private set; }
+    public T Peek { get { return head.Key; } }
+    public PersistentStack<T> Push(T val)
+        => new PersistentStack<T>(new Node(val, head), Count + 1);
+    public PersistentStack<T> Pop()
+        => new PersistentStack<T>(head.next, Count - 1);
+
+}
