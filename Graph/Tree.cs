5bd3e8bf03ec5f0e30f9b9b0c3ec0fa34ae3b5ac
hibati bati <larchandromeda@gmail.com>
2019-09-18 00:55:22 +0900

    add LCA

--- Graph/Tree.cs
+++ Graph/Tree.cs
@@ -5,25 +5,24 @@ using System.Linq;
 public class Tree
 {
     public List<Pair<long, int>>[] edge;
-    private List<int> etList;
     public int Count { get; }
     public Tree(int num)
     {
         Count = num;
         edge = Enumerable.Repeat(0, num).Select(_ => new List<Pair<long, int>>()).ToArray();
     }
+    private static void swap<T>(ref T v1, ref T v2)
+    { var t = v2; v2 = v1; v1 = t; }
     /// <summary>
     /// 辺を追加します
     /// </summary>
     /// <param name="u"></param>
     /// <param name="v"></param>
     /// <param name="weight"></param>
-    /// <param name="directed">有向辺か</param>
-    public void AddEdge(int u, int v, long weight = 1, bool directed = false)
+    public void AddEdge(int u, int v, long weight = 1)
     {
         edge[u].Add(new Pair<long, int>(weight, v));
-        if (!directed)
-            edge[v].Add(new Pair<long, int>(weight, u));
+        edge[v].Add(new Pair<long, int>(weight, u));
     }
     /// <summary>
     /// 木の直径をO(E)で求める
@@ -33,7 +32,7 @@ public class Tree
     /// <returns></returns>
     public long Diameter()
         => Diameter(Diameter(0).v2).v1;
-    //bfsなのはStackOverFlow対策(ACならdfsでもよい)
+
     private Pair<long, int> Diameter(int st)
     {
         var dist = Enumerable.Repeat(-1L, Count).ToArray();
@@ -55,13 +54,14 @@ public class Tree
         return new Pair<long, int>(dist[maxj], maxj);
     }
 
+    private List<int> etList;
     /// <summary>
     /// オイラーツアー
     /// </summary>
     /// <param name="tree"></param>
     /// <param name="root"></param>
     /// <returns></returns>
-    public List<int> EularTour(int root)
+    public List<int> EularTour(int root = 0)
     {
         etList = new List<int>(Count * 2);
         EularTour(root, -1);
@@ -75,6 +75,59 @@ public class Tree
                 EularTour(c.v2, index);
         etList.Add(index);
     }
+
+    private int[][] parent;
+    private int[] depth;
+    /// <summary>
+    /// ダブリングで祖先テーブルを構築します
+    /// 計算量:O(VlogV)
+    /// </summary>
+    /// <param name="root">根</param>
+    public void LCABuild(int root = 0)
+    {
+        int ct = 0;
+        for (var i = 31; i >= 0; i--)
+            if ((1 & Count >> i) == 1)
+            { ct = i; break; }
+        parent = Enumerable.Repeat(0, ct + 1).Select(_ => Enumerable.Repeat(-1, Count).ToArray()).ToArray();
+        depth = new int[Count];
+        LCAdfs(root, -1);
+        for (var i = 1; i <= ct; i++)
+            for (var j = 0; j < Count; j++)
+                if (parent[i - 1][j] != -1)
+                    parent[i][j] = parent[i - 1][parent[i - 1][j]];
+    }
+    private void LCAdfs(int index, int pa)
+    {
+        parent[0][index] = pa;
+        foreach (var e in edge[index])
+            if (e.v2 != pa)
+            {
+                depth[e.v2] = depth[index] + 1;
+                LCAdfs(e.v2, index);
+            }
+    }
+    /// <summary>
+    /// 二頂点の最小共通祖先を求めます
+    /// 計算量:O(logV)
+    /// </summary>
+    /// <param name="u"></param>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    public int LCA(int u, int v)
+    {
+        if (depth[u] > depth[v])
+            swap(ref u, ref v);
+        for (var i = parent.Length - 1; i >= 0; i--)
+            if ((1 & (depth[v] - depth[u]) >> i) == 1)
+                v = parent[i][v];
+        if (u == v) return u;
+        for (var i = parent.Length - 1; i >= 0; i--)
+            if (parent[i][u] != parent[i][v])
+            { u = parent[i][u]; v = parent[i][v]; }
+        return parent[0][u];
+    }
+
 }
 
 public class Pair<T1, T2> : IComparable<Pair<T1, T2>>
