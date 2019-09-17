1e432999ba5ecfe1a83a22045ef08d8ab10ad6d9
hibati bati <larchandromeda@gmail.com>
2019-09-17 23:55:11 +0900

    add

---
+++ DataStructure/RadixHeap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructure
+{
+    using C = System.Int32;
+    public class RadixHeap<T>
+    {
+        public int Count { get; set; }
+        private C last;
+        private List<Pair<C, T>>[] v;
+        public RadixHeap()
+        {
+            v = Enumerable.Repeat(0, Convert.ToString(C.MaxValue, 2).Length + 2).Select(_ => new List<Pair<C, T>>()).ToArray();
+        }
+        private int GetBit(C a)
+        {
+            for (var i = Convert.ToString(C.MaxValue, 2).Length; i >= 0; i--)
+                if ((1 & a >> i) == 1)
+                    return i;
+            return 0;
+        }
+        public void Push(Pair<C, T> p)
+            => Push(p.v1, p.v2);
+        /// <summary>
+        /// last(=最後にpopしたもののkey)以上のkeyについて(key,value)をpushします
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Push(C key, T value)
+        {
+            Count++;
+            v[GetBit(key ^ last)].Add(new Pair<C, T>(key, value));
+        }
+        public Pair<C, T> Pop()
+        {
+            if (v[0].Count == 0)
+            {
+                var ind = 1;
+                while (v[ind].Count == 0) ind++;
+                last = v[ind].Min().v1;
+                foreach (var p in v[ind])
+                    v[GetBit(p.v1 ^ last)].Add(p);
+                v[ind].Clear();
+            }
+            Count--;
+            var ret = v[0].Last();
+            v[0].RemoveAt(v[0].Count - 1);
+            return ret;
+        }
+    }
+}
