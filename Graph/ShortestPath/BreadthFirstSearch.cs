dc219d2c03bfcae9408a0464e713bf471550fea7
hibati bati <larchandromeda@gmail.com>
2019-10-19 17:35:12 +0900

    update

--- Graph/ShortestPath/BreadthFirstSearch.cs
+++ Graph/ShortestPath/BreadthFirstSearch.cs
@@ -19,12 +19,15 @@ namespace Graph
         /// </summary>
         /// <param name="st"></param>
         /// <returns></returns>
-        public int[] Execute(int st = 0)
+        public int[] Execute(int[] st)
         {
             var dist = Create(g.Count, () => -1);
             var que = new Queue<int>();
-            que.Enqueue(st);
-            dist[st] = 0;
+            foreach (var f in st)
+            {
+                que.Enqueue(f);
+                dist[f] = 0;
+            }
             while (que.Any())
             {
                 var p = que.Dequeue();
@@ -43,6 +46,16 @@ namespace Graph
             public BfsEdge(int from, int to) : base(from, to)
             { }
         }
+    }
+    public class BreadthFirstSearch<V> where V : IVertex
+    {
+        private BreadthFirstSearch g;
+        public BreadthFirstSearch(int count)
+        { g = new BreadthFirstSearch(count); }
+        public void AddEdge(V from, V to)
+            => g.AddEdge(from.Id, to.Id);
+        public int[] Execute(V[] st)
+            => g.Execute(st.Select(a => a.Id).ToArray());
     }
     #endregion
 }
