cd6dd511c3cd5cda2ca603ee0afd0f7f3a7a5818
hibati bati <larchandromeda@gmail.com>
2019-10-20 19:45:55 +0900

    update

--- Graph/ShortestPath/Dijkstra.cs
+++ Graph/ShortestPath/Dijkstra.cs
@@ -8,39 +8,60 @@ namespace Graph
 {
     using Number = System.Int64;
     #region ダイクストラ
+    public abstract class DijEdge<W> : Edge, INNegWeight<W> where W : IComparable<W>
+    {
+        public static W Zero, Inf;
+        public W Weight { get; set; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public abstract W Add(W w);
+        public DijEdge(int from, int to, W weight) : base(from, to)
+        { Weight = weight; }
+    }
+
     public class Dijkstra
     {
-        private Graph<DijEdge> g;
-        public Dijkstra(int c) { g = new Graph<DijEdge>(c); }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Dijkstra<Number, DijEdge> dij;
+        public Dijkstra(int count)
+        { dij = new Dijkstra<Number, DijEdge>(count); }
         public void AddEdge(int from, int to, Number weight)
-         => g.Edges[from].Add(new DijEdge(from, to, weight));
-        public Number[] Calc(int st = 0)
+            => dij.AddEdge(new DijEdge(from, to, weight));
+        public Number[] Execute(int st = 0)
+            => dij.Execute(st);
+        public class DijEdge : DijEdge<Number>
         {
-            var pq = new PriorityQueue<Pair<Number, int>>(false);
-            pq.Enqueue(new Pair<Number, int>(0, st));
-            var dist = Create(g.Count, () => Number.MaxValue);
-            dist[st] = 0;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public override long Add(Number w)
+                => Weight + w;
+            public DijEdge(int from, int to, Number weight) : base(from, to, weight)
+            { Zero = 0; Inf = Number.MaxValue; }
+        }
+    }
+    public class Dijkstra<W, DEdge> where W : IComparable<W> where DEdge : DijEdge<W>
+    {
+        private Graph<DEdge> g;
+        public Dijkstra(int c) { g = new Graph<DEdge>(c); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddEdge(DEdge e)
+         => g.Edges[e.From].Add(e);
+        public W[] Execute(int st = 0)
+        {
+            var pq = new PriorityQueue<Pair<W, int>>(false);
+            pq.Enqueue(new Pair<W, int>(DijEdge<W>.Zero, st));
+            var dist = Create(g.Count, () => DijEdge<W>.Inf);
+            dist[st] = DijEdge<W>.Zero;
             var use = new bool[g.Count];
             while (pq.Count != 0)
             {
                 var p = pq.Dequeue();
-                if (p.v1 > dist[p.v2] || use[p.v2]) continue;
+                if (p.v1.CompareTo(dist[p.v2]) == 1 || use[p.v2]) continue;
                 use[p.v2] = true;
                 foreach (var e in g.Edges[p.v2])
-                    if (!use[e.To] && chmin(ref dist[e.To], p.v1 + e.Weight))
-                        pq.Enqueue(new Pair<Number, int>(dist[e.To], e.To));
+                    if (!use[e.To] && chmin(ref dist[e.To], e.Add(p.v1)))
+                        pq.Enqueue(new Pair<W, int>(dist[e.To], e.To));
             }
             return dist;
         }
-
-        public class DijEdge : Edge, INNegWeight<Number>
-        {
-            public Number Weight { get; set; }
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public DijEdge(int from, int to, Number weight) : base(from, to)
-            { Weight = weight; }
-        }
     }
+
     #endregion
 }
