98a7884649811fc85ec955781f1510636e9ad17f
hibati bati <larchandromeda@gmail.com>
2019-10-21 00:09:31 +0900

    update

--- Graph/Template.cs
+++ Graph/Template.cs
@@ -21,6 +21,29 @@ namespace Graph
     { public Directed(int count) : base(count) { } }
     public class Undirected<E> : Graph<E> where E : Edge
     { public Undirected(int count) : base(count) { } }
+    public class Graph<V, E> where V : IVertex where E : IEdge<V>
+    {
+        public List<E>[] Edges { get; set; }
+        public int Count { get; }
+        public Graph(int count)
+        {
+            Count = count;
+            Edges = Create(count, () => new List<E>());
+        }
+    }
+    public class Dynamic<V, E> where V : IVertex where E : IEdge<V>
+    {
+        private Graph<V, E> g;
+        public List<E>[] Edges { get { return g.Edges; } }
+        public int Count { get; }
+        public void AddEdge(E e)
+            => g.Edges[e.From.Id].Add(e);
+        public Dynamic(int count, int pre)
+        {
+            Count = count;
+            g = new Graph<V, E>(pre);
+        }
+    }
 
     public interface IWeight<T> where T : IComparable<T> { T Weight { get; set; } T Add(T t); }
     public interface INNegWeight<T> : IWeight<T> where T : IComparable<T> { }
@@ -33,12 +56,25 @@ namespace Graph
         int From { get; set; }
         int To { get; set; }
     }
+    public interface IEdge<V> where V : IVertex
+    {
+        V From { get; set; }
+        V To { get; set; }
+    }
     public class Edge : IEdge
     {
         public int From { get; set; }
         public int To { get; set; }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Edge(int from, int to)
+        { From = from; To = to; }
+    }
+    public class Edge<V> : IEdge<V> where V : IVertex
+    {
+        public V From { get; set; }
+        public V To { get; set; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Edge(V from, V to)
         { From = from; To = to; }
     }
     #endregion
