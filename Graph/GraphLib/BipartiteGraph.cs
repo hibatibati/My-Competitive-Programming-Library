822f600d8c018cf6610e1f118c42c2ae175bb366
hibati bati <larchandromeda@gmail.com>
2019-10-24 18:30:24 +0900

    add

---
+++ Graph/GraphLib/BipartiteGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// 二部グラフ判定
+/// 依存:UnionFind
+/// </summary>
+public class BipartiteGraph : UnionFind
+{
+    int num;
+    public bool this[int i] { get { return IsSame(0, i); } }
+    public BipartiteGraph(int num) : base(num << 1)
+    { this.num = num; }
+    public bool Coloring()
+    {
+        for (var i = 0; i < num; i++)
+            if (Find(i) == Find(i + num)) return false;
+        return true;
+    }
+    public override bool Union(int v1, int v2)
+        => base.Union(v1, v2 + num) && base.Union(v1 + num, v2);
+}
