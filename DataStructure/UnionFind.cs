810bd3ae9813850f0d7749de0cd72100faedd288
hibati bati <larchandromeda@gmail.com>
2019-10-24 17:46:33 +0900

    fix

--- DataStructure/UnionFind.cs
+++ DataStructure/UnionFind.cs
@@ -4,37 +4,28 @@
 /// </summary>
 public class UnionFind
 {
-    private int _num;
-    private int[] _parent, _size, _rank;
+    protected int[] parent, size, rank;
     public UnionFind(int num)
     {
-        _num = num;
-        _parent = new int[num]; _size = new int[num]; _rank = new int[num];
-        for (var i = 0; i < num; i++) { _parent[i] = i; _size[i] = 1; }
+        parent = new int[num]; size = new int[num]; rank = new int[num];
+        for (var i = 0; i < num; i++) { parent[i] = i; size[i] = 1; }
     }
-    private int Find(int i)
-        => i == _parent[i] ? i : (_parent[i] = Find(_parent[i]));
+    protected int Find(int i)
+        => i == parent[i] ? i : (parent[i] = Find(parent[i]));
     public int Parent(int i)
         => Find(i);
     public int Size(int i)
-        => _size[Find(i)];
+        => size[Find(i)];
     public bool Union(int v1, int v2)
     {
         v1 = Find(v1); v2 = Find(v2);
         if (v1 == v2) return false;
-        _num--;
-        if (_rank[v1] > _rank[v2])
-        {
-            _parent[v2] = v1;
-            _size[v1] += _size[v2];
-        }
-        else
-        {
-            _parent[v1] = v2;
-            _size[v2] += _size[v1];
-            if (_rank[v1] == _rank[v2])
-                _rank[v2]++;
-        }
+        if (rank[v1] < rank[v2])
+            swap(ref v1, ref v2);
+        parent[v2] = v1;
+        size[v1] += size[v2];
+        if (rank[v1] == rank[v2])
+            rank[v1]++;
         return true;
     }
     public bool IsSame(int v1, int v2)
