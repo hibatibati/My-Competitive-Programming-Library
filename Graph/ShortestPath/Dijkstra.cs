3b24f04b8316e2e6f1eddea917dd36634ec178c5
hibati bati <larchandromeda@gmail.com>
2019-10-29 21:31:51 +0900

    fix

--- Graph/ShortestPath/Dijkstra.cs
+++ Graph/ShortestPath/Dijkstra.cs
@@ -10,24 +10,24 @@ using System.Runtime.CompilerServices;
 class Dijkstra
 {
     private int num;
-    private List<Pair<int, Number>>[] edges;
+    private List<Pair<Number, int>>[] edges;
     public Dijkstra(int num)
-    { this.num = num; edges = Create(num, () => new List<Pair<int, Number>>()); }
+    { this.num = num; edges = Create(num, () => new List<Pair<Number, int>>()); }
     public void AddEdge(int from, int to, Number weight)
-        => edges[from].Add(new Pair<int, Number>(to, weight));
+        => edges[from].Add(new Pair<Number, int>(weight, to));
     public Number[] Execute(int st = 0)
     {
         var dist = Create(num, () => Number.MaxValue);
-        var pq = new PriorityQueue<Pair<int, Number>>((a, b) => a.v2.CompareTo(b.v2));
-        pq.Enqueue(new Pair<int, Number>(st, 0));
+        var pq = new PriorityQueue<Pair<Number, int>>((a, b) => a.v1.CompareTo(b.v1));
+        pq.Enqueue(new Pair<Number, int>(0, st));
         dist[st] = 0;
         while (pq.Any())
         {
             var p = pq.Dequeue();
-            if (p.v2 > dist[p.v1]) continue;
-            foreach (var e in edges[p.v1])
-                if (chmin(ref dist[e.v1], e.v2 + p.v2))
-                    pq.Enqueue(new Pair<int, Number>(e.v1, dist[e.v1]));
+            if (p.v1 > dist[p.v2]) continue;
+            foreach (var e in edges[p.v2])
+                if (chmin(ref dist[e.v2], e.v1 + p.v1))
+                    pq.Enqueue(new Pair<Number, int>(dist[e.v2], e.v2));
         }
         return dist;
     }
