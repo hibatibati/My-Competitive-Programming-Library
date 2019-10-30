d192b0ea387e2a1687991c3313d3066b3d6bc097
hibati bati <larchandromeda@gmail.com>
2019-10-30 20:46:29 +0900

    update

--- DataStructure/UnionFind.cs
+++ DataStructure/UnionFind.cs
@@ -1,33 +1,28 @@
 ﻿using System;
 /// <summary>
-/// Union/IsSame:O(α(N))
+/// Union:O(α(N))
 /// </summary>
 public class UnionFind
 {
-    protected int[] parent, size, rank;
+    protected int[] parent, size;
+    public virtual int this[int i] { get { return Find(i); } }
     public UnionFind(int num)
     {
-        parent = new int[num]; size = new int[num]; rank = new int[num];
+        parent = new int[num]; size = new int[num];
         for (var i = 0; i < num; i++) { parent[i] = i; size[i] = 1; }
     }
     protected int Find(int i)
         => i == parent[i] ? i : (parent[i] = Find(parent[i]));
-    public int Parent(int i)
-        => Find(i);
     public int Size(int i)
         => size[Find(i)];
     public virtual bool Union(int v1, int v2)
     {
         v1 = Find(v1); v2 = Find(v2);
         if (v1 == v2) return false;
-        if (rank[v1] < rank[v2])
+        if (size[v1] < size[v2])
             swap(ref v1, ref v2);
         parent[v2] = v1;
         size[v1] += size[v2];
-        if (rank[v1] == rank[v2])
-            rank[v1]++;
         return true;
     }
-    public bool IsSame(int v1, int v2)
-        => Find(v1) == Find(v2);
 }
