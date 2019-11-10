4fa8275079cf173efe51b6414518b52291c7a488
hibati bati <larchandromeda@gmail.com>
2019-11-10 14:23:31 +0900

    add

---
+++ Graph/MST/Prim.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.CompilerServices;
+/// <summary>
+/// 最小全域木を求める
+/// 依存:PriorityQueue
+/// </summary>
+class Prim
+{
+    private int num;
+    private List<Edge>[] edges;
+    public Prim(int num)
+    { this.num = num; edges = Create(num, () => new List<Edge>()); }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddEdge(int from, int to, Number weight)
+        => edges[from].Add(new Edge(to, weight));
+    public Number Execute(int st = 0)
+    {
+        Number res = 0;
+        var pq = new PriorityQueue<Edge>();
+        pq.Enqueue(new Edge(st, 0));
+        var use = new bool[num];
+        while (pq.Any())
+        {
+            var p = pq.Dequeue();
+            if (use[p.to]) continue;
+            use[p.to] = true;
+            res += p.cost;
+            foreach (var e in edges[p.to])
+                if (!use[e.to])
+                    pq.Enqueue(e);
+        }
+        return res;
+    }
+    private struct Edge : IComparable<Edge>
+    {
+        public int to;
+        public Number cost;
+        public Edge(int to, Number cost)
+        { this.to = to; this.cost = cost; }
+        public int CompareTo(Edge e)
+            => cost.CompareTo(e.cost);
+    }
+}
