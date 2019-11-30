04d22deb4058834a497c6bd7b7ba08d248ba3c07
hibati bati <larchandromeda@gmail.com>
2019-11-30 18:24:37 +0900

    update

---
+++ DataStructure/UnionFind/PartiallyPersistentUnionFind.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PartiallyPersistentUnionFind
+{
+    private int now = -1;
+    protected int[] data, time;
+    List<Tuple<int, int>>[] size;
+    public virtual int this[int t, int i] { get { return Find(t, i); } }
+    public PartiallyPersistentUnionFind(int size)
+    {
+        data = Create(size, () => -1);
+        this.size = Create(size, () => new List<Tuple<int, int>> { new Tuple<int, int>(-1, 1) });
+        time = Create(size, () => int.MaxValue);
+    }
+    protected int Find(int t, int i)
+    {
+        if (time[i] > t) return i;
+        return Find(t, data[i]);
+    }
+    public int Size(int t, int x)
+    {
+        x = Find(t, x);
+        int r = size[x].Count, l = -1;
+        while (r - l > 1)
+        {
+            var m = (r + l) / 2;
+            if (size[x][m].Item1 > t) r = m;
+            else l = m;
+        }
+        return size[x][l].Item2;
+    }
+    public virtual bool Union(int u, int v)
+    {
+        now++;
+        u = Find(now, u); v = Find(now, v);
+        if (u == v) return false;
+        if (data[u] > data[v])
+            swap(ref u, ref v);
+        data[u] += data[v];
+        size[u].Add(new Tuple<int, int>(now, -data[u]));
+        data[v] = u;
+        time[v] = now;
+        return true;
+    }
+}
---
+++ DataStructure/UnionFind/UndoableUnionFind.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class UndoableUnionFind
+{
+    public int GroupCount { get; private set; }
+    protected int[] data;
+    private Stack<Tuple<int, int>> history;
+    public virtual int this[int i] { get { return Find(i); } }
+    public UndoableUnionFind(int size)
+    {
+        data = Create(size, () => -1);
+        history = new Stack<Tuple<int, int>>();
+        GroupCount = size;
+    }
+    protected int Find(int i)
+        => data[i] < 0 ? i : Find(data[i]);
+    public int Size(int i)
+        => -data[Find(i)];
+    public virtual bool Union(int u, int v)
+    {
+        u = Find(u); v = Find(v);
+        history.Push(new Tuple<int, int>(u, data[u]));
+        history.Push(new Tuple<int, int>(v, data[v]));
+        if (u == v) return false;
+        if (data[u] > data[v])
+            swap(ref u, ref v);
+        GroupCount--;
+        data[u] += data[v];
+        data[v] = u;
+        return true;
+    }
+    public void Undo()
+    {
+        if (!history.Any()) return;
+        data[history.Peek().Item1] = history.Pop().Item2;
+        data[history.Peek().Item1] = history.Pop().Item2;
+    }
+    public void SnapShot() => history.Clear();
+    public void RollBack()
+    {
+        while (history.Any()) Undo();
+    }
+}
