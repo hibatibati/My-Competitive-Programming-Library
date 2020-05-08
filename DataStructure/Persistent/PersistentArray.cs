4505edc2da81b047bc07ac851c3d44a03cf2a149
hibati <larchandromeda@gmail.com>
2020-05-08 15:15:11 +0900

    add

---
+++ DataStructure/Persistent/PersistentArray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+//verifyしていません
+public class PersistentArray<T>
+{
+    public int Length { get; private set; }
+    Node root;
+    public PersistentArray(int sz)
+    {
+        Length = sz;
+        root = new Node { idx = 1 };
+        var Q = new Queue<Node>();
+        if (2 <= sz)
+            Q.Enqueue(root);
+        while (Q.Any())
+        {
+            var p = Q.Dequeue();
+            p.lft = new Node() { idx = 1 << p.idx };
+            if ((p.lft.idx << 1) <= sz) Q.Enqueue(p.lft);
+            if (((p.idx << 1) | 1) <= sz)
+            {
+                p.rgt = new Node() { idx = (p.idx << 1) | 1 };
+                if ((p.rgt.idx << 1) <= sz) Q.Enqueue(p.rgt);
+            }
+        }
+    }
+    PersistentArray(Node r, int sz) { root = r; Length = sz; }
+    public T this[int i]
+    {
+        get
+        {
+            var mask = MSB(++i) - 1;
+            var nd = root;
+            while (mask >= 0) { if ((1 & i >> mask) == 0) nd = nd.lft; else nd = nd.rgt; mask--; }
+            return nd.dat;
+        }
+    }
+    public PersistentArray<T> Set(int i, T v)
+    {
+        var nd = root.DeepCopy();
+        var mask = MSB(++i) - 1;
+        var r = new PersistentArray<T>(nd, Length);
+        while (mask >= 0)
+        {
+            if ((1 & i >> mask) == 0) nd = nd.lft = nd.lft.DeepCopy();
+            else nd = nd.rgt = nd.rgt.DeepCopy();
+            mask--;
+        }
+        nd.dat = v;
+        return r;
+    }
+    class Node
+    {
+        public int idx;
+        public T dat;
+        public Node lft, rgt;
+        public Node DeepCopy() => new Node() { idx = idx, dat = dat, lft = lft, rgt = rgt };
+    }
+
+    int MSB(int v)
+    {
+        var rt = 0;
+        if ((v >> 16) > 0) { rt |= 16; v >>= 16; }
+        if ((v >> 8) > 0) { rt |= 8; v >>= 8; }
+        if ((v >> 4) > 0) { rt |= 4; v >>= 4; }
+        if ((v >> 2) > 0) { rt |= 2; v >>= 2; }
+        return rt | (v >> 1);
+    }
+}
---
+++ DataStructure/SegmentTree/SegmentTreeFractionalCascading.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SegmentTreeFractionalCascading
+{
+    Tuple<int, long>[][] seg;
+    int[][] L, R;
+    long[][] sum;
+    int sz;
+    public SegmentTreeFractionalCascading(List<Tuple<int, long>>[] A)
+    {
+        sz = 1;
+        while (sz < A.Length) sz <<= 1;
+        seg = new Tuple<int, long>[2 * sz - 1][];
+        L = new int[2 * sz - 1][];
+        R = new int[2 * sz - 1][];
+        sum = new long[2 * sz - 1][];
+        for (int i = 0; i < sz; i++)
+        {
+            if (i < A.Length)
+            {
+                seg[i + sz - 1] = A[i].ToArray();
+                sum[i + sz - 1] = new long[seg[i + sz - 1].Length + 1];
+                Array.Sort(seg[i + sz - 1], (a, b) => a.Item1 - b.Item1);
+                for (int j = 0; j < seg[i + sz - 1].Length; j++)
+                    sum[i + sz - 1][j + 1] = sum[i + sz - 1][j] + seg[i + sz - 1][j].Item2;
+            }
+            else
+            {
+                seg[i + sz - 1] = new Tuple<int, long>[0];
+                sum[i + sz - 1] = new long[1];
+            }
+        }
+        for (int k = sz - 2; k >= 0; k--)
+        {
+            seg[k] = new Tuple<int, long>[seg[2 * k + 1].Length + seg[2 * k + 2].Length];
+            L[k] = new int[seg[k].Length + 1];
+            R[k] = new int[seg[k].Length + 1];
+            sum[k] = new long[seg[k].Length + 1];
+            int t1 = 0, t2 = 0;
+            for (int i = 0; i < seg[k].Length; i++)
+            {
+                L[k][i] = t1;
+                R[k][i] = t2;
+                if (t1 == seg[2 * k + 1].Length)
+                {
+                    seg[k][i] = seg[2 * k + 2][t2++];
+                }
+                else if (t2 == seg[2 * k + 2].Length)
+                {
+                    seg[k][i] = seg[2 * k + 1][t1++];
+                }
+                else
+                {
+                    if (seg[2 * k + 1][t1].Item1 < seg[2 * k + 2][t2].Item1)
+                        seg[k][i] = seg[2 * k + 1][t1++];
+                    else
+                        seg[k][i] = seg[2 * k + 2][t2++];
+                }
+                sum[k][i + 1] = sum[k][i] + seg[k][i].Item2;
+            }
+            L[k][seg[k].Length] = seg[2 * k + 1].Length;
+            R[k][seg[k].Length] = seg[2 * k + 2].Length;
+        }
+    }
+    public long Query(int l, int r, int d, int u)
+    {
+        d = LowerBound(seg[0], new Tuple<int, long>(d, -1), (a, b) => a.Item1.CompareTo(b.Item1));
+        u = LowerBound(seg[0], new Tuple<int, long>(u, -1), (a, b) => a.Item1.CompareTo(b.Item1));
+        return Query(l, r, d, u, 0, 0, sz);
+    }
+    long Query(int l, int r, int lw, int up, int k, int lft, int rgt)
+    {
+        if (rgt <= l || r <= lft) return 0;
+        if (l <= lft && rgt <= r) return sum[k][up] - sum[k][lw];
+        return Query(l, r, L[k][lw], L[k][up], 2 * k + 1, lft, (lft + rgt) >> 1) + Query(l, r, R[k][lw], R[k][up], 2 * k + 2, (lft + rgt) >> 1, rgt);
+    }
+    #region UpperBound/LowerBound
+    public static int UpperBound<T>(IList<T> array, T value, Comparison<T> cmp = null)
+    {
+        cmp = cmp ?? Comparer<T>.Default.Compare;
+        var low = -1;
+        var high = array.Count;
+        while (high - low > 1)
+        {
+            var mid = (high + low) / 2;
+            if (cmp(array[mid], value) == 1) high = mid;
+            else low = mid;
+        }
+        return high;
+    }
+
+    public static int LowerBound<T>(IList<T> array, T value, Comparison<T> cmp = null)
+    {
+        cmp = cmp ?? Comparer<T>.Default.Compare;
+        var low = -1;
+        var high = array.Count;
+        while (high - low > 1)
+        {
+            var mid = (high + low) / 2;
+            if (cmp(array[mid], value) != -1) high = mid;
+            else low = mid;
+        }
+        return high;
+    }
+    #endregion
+}
