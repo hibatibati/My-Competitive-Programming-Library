3713793962ddb52e9c780777b4136322a9e5a14a
hibati <larchandromeda@gmail.com>
2020-08-19 01:02:12 +0900

    update

--- DataStructure/BinaryIndexedTree.cs
+++ DataStructure/BinaryIndexedTree.cs
@@ -3,17 +3,46 @@
 public class BIT
 {
     //1-indexed
-    long[] data;
-    public BIT(int num)
+    int bit;
+    long[] dat;
+    public BIT(int N)
     {
-        data = new long[num + 1];
+        dat = new long[N + 1];
+        bit = 1;
+        while ((bit << 1) <= N) bit <<= 1;
     }
-    public long this[int index]
+    public long this[int idx]
     {
-        get { var s = 0L; for (var i = index; i > 0; i -= i & -i) s += data[i]; return s; }
+        get { long s = 0; for (var i = idx; i > 0; i -= i & -i) s += dat[i]; return s; }
     }
-    public void add(int index, long value)
+    public void add(int idx, long v)
     {
-        for (var i = index; i < data.Length; i += i & -i) data[i] += value;
+        for (var i = idx; i < dat.Length; i += i & -i) dat[i] += v;
+    }
+    public int LowerBound(long v)
+    {
+        int now = 0;
+        for(int k = bit; k > 0; k >>= 1)
+        {
+            if (now + k < dat.Length && dat[now + k] < v)
+            {
+                v -= dat[now + k];
+                now += k;
+            }
+        }
+        return now+1;
+    }
+    public int UpperBound(long v)
+    {
+        int now = 0;
+        for (int k = bit; k > 0; k >>= 1)
+        {
+            if (now + k < dat.Length && dat[now + k] <= v)
+            {
+                v -= dat[now + k];
+                now += k;
+            }
+        }
+        return now+1;
     }
 }
